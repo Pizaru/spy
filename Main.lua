a98cfe0db3bfcbb5fc95c00da548a23e18dd5f2f
Pizz Kun <hapizdnuryadin1412@gmail.com>
2025-11-08 15:03:36 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@
 --// File handling configuration 
 local FilesConfig = {
 	UseWorkspace = false,
-	RepoUrl = "https://raw.githubusercontent.com/Pizaru/Spy/refs/heads/main" -- "http://127.0.0.1:3000"
+	RepoUrl = "https://raw.githubusercontent.com/Pizaru/spy/refs/heads/main" -- "http://127.0.0.1:3000"
 }
 
 --// Service handlers
