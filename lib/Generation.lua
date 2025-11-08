2a9adba93b79b87606593be2aa3d1811b75e63b8
Pizz Kun <hapizdnuryadin1412@gmail.com>
2025-11-08 16:56:49 +0700

    Update Generation.lua

--- lib/Generation.lua
+++ lib/Generation.lua
@@ -5,7 +5,7 @@ type table = {
 }
 
 --// Libraries
-local ParserModule = loadstring(game:HttpGet('https://raw.githubusercontent.com/Pizaru/parser/refs/heads/main/main.lua'))()
+local ParserModule = loadstring(game:HttpGet('https://raw.githubusercontent.com/Pizaru/parser/refs/heads/main/main.luau'))()
 
 --// Parser
 function ParserModule:Import(Name: string)
